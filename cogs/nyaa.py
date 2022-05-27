b218d352391df4c5138aef1c5ec142891e50fc3d
loogabooga <veldigbraepost@icloud.com>
2022-05-27 11:46:30 +0200

    nya~
    Co-authored-by: Radical <Radiicall@users.noreply.github.com>

---
+++ cogs/nyaa.py
@@ -0,0 +1,44 @@
+import discord
+from discord.ext import commands
+import NyaaPy.nyaa as Nyaa
+
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+def sort(List):
+    List = list(List)
+    x = 0
+    for i in range(len(List)):
+        y = dict(List[i])
+        y = int(y["seeders"])
+        if y > x:
+            x = y
+            print(f"x: {x}, y: {y}")
+            List.insert(0, List.pop(i))
+
+    return List
+
+class nyaa(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    @commands.command()
+    async def nyaa(self, ctx, *, query):
+        result = Nyaa.Nyaa.search(keyword=query,category=1)
+        result = sort(result)
+        name = "```"
+        for i in range(15):
+            try:
+                name = name + f"{i+1}. {str(result[i]['name'])}\nId: {str(result[i]['id'])}\nSeeders: {str(result[i]['seeders'])}\n\n"
+            except:
+                pass
+        name = name + "```"
+        await ctx.send(name)
+    @commands.command()
+    async def hash(self, ctx, *, id):
+        result = Nyaa.Nyaa.get(id)
+        await ctx.send(result["hash"])
+
+
+def setup(bot):
+    bot.add_cog(nyaa(bot))
--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-the
+# import discord.py
+import discord
+from discord.ext import commands
+import os
+import json
+
+
+with open('cnofig.json') as f:
+    config = json.load(f)
+
+
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.command()
+async def reload(ctx):
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            bot.reload_extension(f'cogs.{filename[:-3]}')
+            print(f'Reloaded {filename}')
+
+# coghgers
+for filename in os.listdir('cogs'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
+
+bot.run(config['token'], bot=True, reconnect=True)
