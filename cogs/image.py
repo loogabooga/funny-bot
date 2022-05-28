2ee05aa7b97d03e3834c8d1a29d80cdd47916780
loogabooga <93138274+loogabooga@users.noreply.github.com>
2022-05-28 18:30:02 +0200

    Added unsupported image error message

--- cogs/image.py
+++ cogs/image.py
@@ -13,22 +13,30 @@ class image(commands.Cog):
         # get image from message attachment
         if ctx.message.attachments:
             url = ctx.message.attachments[0].url
-            response = requests.get(url)
-            image = cv2.imdecode(np.fromstring(response.content, np.uint8), cv2.IMREAD_COLOR)
+            if url.endswith==".png" or url.endswith==".jpg" or url.endswith==".jpeg":
+                response = requests.get(url)
+                image = cv2.imdecode(np.fromstring(response.content, np.uint8), cv2.IMREAD_COLOR)
+            else:
+                await ctx.send("Please upload a .png, .jpg, or .jpeg image.")
+                return
         # get image from url
         else:
             url = query
-            response = requests.get(url)
-            image = cv2.imdecode(np.fromstring(response.content, np.uint8), cv2.IMREAD_COLOR)
+            if url.endswith==".png" or url.endswith==".jpg" or url.endswith==".jpeg":
+                response = requests.get(url)
+                image = cv2.imdecode(np.fromstring(response.content, np.uint8), cv2.IMREAD_COLOR)
+            else:
+                await ctx.send("Please upload a .png, .jpg, or .jpeg image.")
+                return
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         # detect edges
         edges = cv2.Canny(gray, 100, 200)
         # display image
-        cv2.imwrite('edge.png', edges)
+        cv2.imwrite('/dev/shm/edge.png', edges)
         # send image
-        await ctx.send(file=discord.File('edge.png'))
+        await ctx.send(file=discord.File('/dev/shm/edge.png'))
         # remove image
-        remove('edge.png')
+        remove('/dev/shm/edge.png')
 
 def setup(bot):
     bot.add_cog(image(bot))
